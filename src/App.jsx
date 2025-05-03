cd106ea92b8df41d3565adffa2017edc94946ba7
ashwin212004 <ashwingonbhare@gmail.com>
2025-05-03 08:48:43 +0530

    redux commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodo from './Componets/AddTodo'
+import Todos from './Componets/Todos'
+
+function App() {
+
+  return (
+    <>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
